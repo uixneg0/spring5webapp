addb31e34e69e3d364c305737a04c1d6067760db
uixneg0 <96099199+uixneg0@users.noreply.github.com>
2022-11-29 22:31:54 -0600

    done

---
+++ src/main/java/guru/springframework/spring5webapp/entities/Publisher.java
@@ -0,0 +1,57 @@
+package guru.springframework.spring5webapp.entities;
+
+import javax.persistence.*;
+import java.util.Objects;
+
+@Entity
+public class Publisher {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id", nullable = false)
+    private Long id;
+    @OneToOne
+    @JoinColumn(name = "address_id")
+    private Address address;
+    private String name;
+
+    public Publisher(String name, Address address) {
+        this.name = name;
+        this.address = address;
+    }
+
+    public Publisher() {
+
+    }
+
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Publisher publisher = (Publisher) o;
+
+        return Objects.equals(id, publisher.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return id != null ? id.hashCode() : 0;
+    }
+}
---
+++ src/main/java/guru/springframework/spring5webapp/repos/PublisherRepository.java
@@ -0,0 +1,15 @@
+package guru.springframework.spring5webapp.repos;
+
+import guru.springframework.spring5webapp.entities.Publisher;
+import org.springframework.data.domain.Example;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface PublisherRepository extends CrudRepository<Publisher, Long> {
+}
