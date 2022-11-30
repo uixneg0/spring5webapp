2d96e76c6f15ffafe16e47816ec2e4d048c80845
uixneg0 <96099199+uixneg0@users.noreply.github.com>
2022-11-29 22:37:09 -0600

    Revert "done"
    This reverts commit addb31e34e69e3d364c305737a04c1d6067760db.

--- src/main/java/guru/springframework/spring5webapp/repos/AddressRepository.java
+++
@@ -1,8 +0,0 @@
-package guru.springframework.spring5webapp.repos;
-
-import guru.springframework.spring5webapp.entities.Address;
-import org.springframework.data.repository.CrudRepository;
-
-public interface AddressRepository extends CrudRepository<Address, Long> {
-
-}
